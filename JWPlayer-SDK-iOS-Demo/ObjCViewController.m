4f6726f272c3d9a4c3f447b5a715b10f9019fc02
David Calhoun <dpcalhoun@gmail.com>
2020-01-13 22:23:18 -0600

    Add ad tag configuration

--- JWPlayer-SDK-iOS-Demo/ObjCViewController.m
+++ JWPlayer-SDK-iOS-Demo/ObjCViewController.m
@@ -22,8 +22,11 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    JWAdBreak *adBreak = [JWAdBreak adBreakWithTag:@"https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dlinear&correlator=" offset:@"pre"];
     JWConfig *config = [JWConfig configWithContentURL:@"http://content.bitsontherun.com/videos/3XnJSIm4-injeKYZS.mp4"];
+    config.advertising = [JWAdConfig new];
+    config.advertising.client = JWAdClientVast;
+    config.advertising.schedule = @[adBreak];
     self.player = [[JWPlayerController alloc]initWithConfig:config];
 }
 
